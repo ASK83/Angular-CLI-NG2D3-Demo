182071d8e270fcf6efce268155ec5a5a04974802
Marjan Georgiev <marjan.georgiev@gmail.com>
2016-11-11 15:01:03 +0100

    Update to ng2d3 1.5.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
     domain: ['#F44336', '#3F51B5', '#8BC34A', '#2196F3', '#009688', '#FF5722', '#CDDC39', '#00BCD4', '#FFC107', '#795548', '#607D8B']
   };
 
-  single = [
+  data = [
     {
       "name": "Germany",
       "value": 8940000
